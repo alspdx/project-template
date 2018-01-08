1a606bff065eddcc3eea40bb0455adfe1f31d1bb
Adam <alspdx@gmail.com>
2018-01-07 23:19:05 -0800

    initialize new project template

---
+++ spec/program-spec.js
@@ -0,0 +1,16 @@
+// import { Module } from './../src/js/module.js';
+//
+// describe('Object', function() {
+//   const arg1 = 'Value 1';
+//   const arg2 = 'Value 2';
+//   let newObject;
+//
+//   beforeEach(function() {
+//     newObject = new Object(arg1, arg2);
+//   });
+//
+//   it('should test whether...', function() {
+//     expect(newObject.method(argument)).toEqual(returnValue);
+//     expect(()).not.toEqual();
+//   });
+// });
